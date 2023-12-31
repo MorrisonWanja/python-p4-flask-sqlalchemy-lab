01559b3fe0b98d0600dc0b8c295cad56392d097a
morrison <morrisonwanja@gmail.com>
2023-12-31 17:18:11 +0300

    Complete Flak-SQL Alchemy Lab

--- server/app.py
+++ server/app.py
@@ -10,7 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/')
@@ -19,15 +18,72 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    if not animal:
+        response_body = '<h1>404 Owner not found</h1>'
+        response = make_response(response_body,404)
+        return response
+    response_body =f'''
+                      <ul>ID:{animal.id}</ul>
+                      <ul>Name.{animal.name} </ul>
+                      <ul>Species.{animal.species} </ul>
+                    '''
+    zookeepers = animal.zookeeper
+    if not zookeepers:
+        response_body += f'<h2>Has no zookeeper currently.</h2>'
+    else:
+        response_body += f''' 
+                          <ul>Zookeeper.{zookeepers.name} </ul>
+                        
+                         '''
+    enclosures = animal.enclosure
+    if not enclosures:
+        response_body += f'<h2>Has no enclosures currently.</h2>'
+    else:
+        response_body += f'<ul>Enclosure.{enclosures.environment} </ul>' 
+
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    birth_day = Zookeeper.query.filter(Zookeeper.id == id).first()
+    if not birth_day:
+        response_body = '<h1>404 Zookeeper not found</h1>'
+        response = make_response(response_body,404)
+        return response
+    response_body = f'''<ul>Name.{birth_day.name} </ul>
+                        <ul>Birthday.{birth_day.birthday}</ul>
+                     '''
+    animals = [animal for animal in birth_day.anim]
+    if not animals:
+        response_body += f'<h2>Has no animals currently.</h2>'
+    else:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name} </ul>'
+   
+    response = make_response(response_body,200)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    if not enclosure:
+        response_body = '<h1>404 Enclosure not found!</h1>'
+        response = make_response(response_body,404) 
+        return response
+    response_body = f'''<ul>Environment.{enclosure.environment} </ul>
+                        <ul>Open to Visitors.{enclosure.open_to_visitors}</ul>
+                      '''  
+    animals = [animal for animal in enclosure.animals]
+    if not animals:
+        response_body += f'<h2>Has no animals currently.</h2>'
+    else:
+        for animal in animals:
+            response_body += f'<ul>Animal.{animal.name} </ul>'
+   
+    response = make_response(response_body,200)
+    return response
 
 
 if __name__ == '__main__':
